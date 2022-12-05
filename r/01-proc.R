94135847a43390b9859d09ba56b9f4a472839e30
SanDBIG <diturrieta.g@gmail.com>
2022-12-05 01:45:51 -0300

    a
    a

---
+++ r/01-proc.R
@@ -0,0 +1,28 @@
+# Cargar Paquetes ---------------------------------------------------------
+
+pacman::p_load(tidyverse,
+               haven,
+               dplyr,
+               magrittr,
+               sjmisc, #Para explorar datos
+               sjPlot, #Para los gráficos
+               ggpubr, #Para incorporar correlación en scatterplot (grid)
+               grid, #Para incorporar correlación en scatterplot
+               ggrepel, #Para evitar solapamientos en gráfico
+               forcats,
+               car,
+               knitr,
+               srvyr,
+               sjlabelled,
+               knitr,
+               srvyr)
+
+# Cargar Datos -------------------------------------------------------------
+septiembre_2022 <- read.csv("input/data/Septiembre-2022.csv", sep=";", 
+                            encoding = "UTF-8", stringsAsFactors = F) # se agrega sep=";" ya que las columnas van separadas con ese caracter
+
+
+
+# Procesamiento -----------------------------------------------------------
+
+
